b098cabf1df9b017ec4debae25cb5a0abfd6cad9
Coeuvre Wong <coeuvre@gmail.com>
2019-07-08 18:03:32 +0800

    Draw wireframe

--- examples/tinyrenderer.rs
+++ examples/tinyrenderer.rs
@@ -1,13 +1,35 @@
+use std::fs::File;
+use std::io::BufReader;
+
 mod support;
 
 use support::canvas::*;
 
 fn main() {
-    setup(|canvas| {
-        canvas.draw_line(100, 100, 200, 300);
-        canvas.draw_line(100, 100, 200, 200);
+    let input =
+        BufReader::new(File::open("data/african_head.obj").expect("Failed to find obj file"));
+    let model: obj::Obj = obj::load_obj(input).expect("Failed to load obj file");
 
-        canvas.fill_triangle(100.0, 100.0, 200.0, 100.0, 190.0, 150.0);
-        canvas.fill_triangle(200.0, 100.0, 400.0, 150.0, 190.0, 150.0);
+    setup(|canvas| {
+        let width = canvas.width();
+        let height = canvas.height();
+        for face in model.indices.chunks(3) {
+            let v0: obj::Vertex = model.vertices[face[0] as usize];
+            let v1: obj::Vertex = model.vertices[face[1] as usize];
+            let v2: obj::Vertex = model.vertices[face[2] as usize];
+
+            let mut draw_line = |a: obj::Vertex, b: obj::Vertex| {
+                canvas.draw_line(
+                    ((a.position[0] + 1.0) * (width as f32) / 2.0).round() as i32,
+                    ((a.position[1] + 1.0) * (height as f32) / 2.0).round() as i32,
+                    ((b.position[0] + 1.0) * (width as f32) / 2.0).round() as i32,
+                    ((b.position[1] + 1.0) * (height as f32) / 2.0).round() as i32,
+                );
+            };
+
+            draw_line(v0, v1);
+            draw_line(v1, v2);
+            draw_line(v2, v0);
+        }
     });
 }
